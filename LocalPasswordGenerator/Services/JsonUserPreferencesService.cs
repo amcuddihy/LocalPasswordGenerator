6e7771d51a56c1b3ab8ece56e2f1916bac21d480
amcuddihy <alexcuddihy@gmail.com>
2025-12-29 17:04:44 -0500

    Cleaned up the code in the Json User Preferences service

--- LocalPasswordGenerator/Services/JsonUserPreferencesService.cs
+++ LocalPasswordGenerator/Services/JsonUserPreferencesService.cs
@@ -4,40 +4,26 @@ using System.IO;
 
 namespace LocalPasswordGenerator.Services;
 
-/// <summary>
-/// Uses the Newtonsoft.Json library to store the user settings as a JSON file.
-/// Implements the IUserPreferencesService interface so that it can be dependency
-/// injected into the PasswordViewModel.
-/// </summary>
 public class JsonUserPreferencesService : IUserPreferencesService
 {
     private static readonly string FilePath = "user_prefs.json";
 
-    /// <summary>
-    /// Loads the user preferences from a JSON file. 
-    /// </summary>
-    /// <returns>The user preferences as a UserPreferences object</returns>
     public PasswordSettings Load() 
     {
-        try 
-        {
-            if (File.Exists(FilePath)) 
-            {
-                string json = File.ReadAllText(FilePath);
-                return JsonConvert.DeserializeObject<PasswordSettings>(json) ?? new PasswordSettings();
-            }
+        try {
+            var rawJsonText = File.ReadAllText(FilePath);
+            var userSettings = JsonConvert.DeserializeObject<PasswordSettings>(rawJsonText);
+
+            return userSettings is null
+                ? new PasswordSettings()
+                : userSettings;
         }
-        catch (Exception ex) 
-        {
+        catch (Exception ex) {
             Console.WriteLine($"Error loading preferences: {ex.Message}");
+            return new PasswordSettings();
         }
-        return new PasswordSettings();
     }
 
-    /// <summary>
-    /// Saves the user preferences as a JSON file.
-    /// </summary>
-    /// <param name="preferences">User preferences object to be serialized into JSON and saved.</param>
     public void Save(PasswordSettings preferences) 
     {
         try 
